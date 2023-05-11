5a98b1b5472518bbdf1facf228ac0810e412942e
OficialNico <112134051+OficialNico@users.noreply.github.com>
2023-05-11 11:42:59 -0400

    arreglando db

--- core/settings.py
+++ core/settings.py
@@ -81,20 +81,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+    
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': '127.0.0.1:1521/xe',
-        'USER': 'c##admin_tienda',
-        'PASSWORD': '123456',
-        'TEST': {
-            'USER': 'default_test',
-            'TBLSPACE': 'default_test_tbls',
-            'TBLSPACE_TMP': 'default_test_tbls_tmp',
-        },
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 # Password validation
