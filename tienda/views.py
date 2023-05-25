f6f739719f09abc724cde2ef2f4c8dd08f361e94
OficialNico <112134051+OficialNico@users.noreply.github.com>
2023-05-25 12:22:55 -0400

    Login

--- tienda/views.py
+++ tienda/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 from .models import Producto
+# views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+
+def login_view(request):
+    if request.method == 'POST':
+        Usuario = request.POST['Usuario']
+        Contraseña = request.POST['Contraseña']
+        user = authenticate(request, Usuario=Usuario, Contraseña=Contraseña)
+        if Usuario is not None:
+            login(request, user)
+            return redirect('home')  # Redirige a la página de inicio
+        else:
+            error_message = 'Usuario o contraseña incorrecta.'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        Usuario = request.POST['Usuario']
+        Contraseña = request.POST['Contraseña']
+        confirme_Contraseña = request.POST['confirme_Contraseña']
+        if Contraseña == confirme_Contraseña:
+            Usuario = Usuario.objects.create_user(Usuario=Usuario, Contraseña=Contraseña)
+            login(request, Usuario)
+            return redirect('home')  # Redirige a la página de inicio
+        else:
+            error_message = 'Contraseña incorrecta.'
+            return render(request, 'signup.html', {'error_message': error_message})
+    else:
+        return render(request, 'signup.html')
+
 
 # Create your views here.
 
