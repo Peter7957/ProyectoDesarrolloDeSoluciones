3fcffc4285951e9a86a428a3aec7467ac7f64140
Pedro Cornejo <pedrocornejo99@gmail.com>
2023-06-02 15:42:34 -0400

    Comenzando carrito

---
+++ tienda/carrito.py
@@ -0,0 +1,43 @@
+class carrito:
+    def __init__(self, request):
+        self.request = request
+        self.session = request.session
+        carrito = self.session["carrito"]
+        if not carrito:
+            self.session["carrito"] = {}
+        else:
+            self.carrito = carrito
+    
+    def agregar(self, producto):
+        id = str(producto.id)
+        if id not in self.carrito.keys():
+            self.carrito[id]={
+                "producto_id"   : producto.id,
+                "nombre"        : producto.combre,
+                "acumulado"     : producto.precio,
+                "cantidad"      : 1
+            }
+        else:
+            self.carrito[id]["cantidad"] += 1
+            self.carrito[id]["acumulado"] += producto.precio
+    
+    def guardar_carrito(self):
+        self.session["carrito"] = self.carritoself.session.modified = True
+    
+    def eliminar(self):
+        id = str(producto.id)
+        if id in self.carrito:
+            del self.carrito[id]
+            self.guardar_carrito()
+
+    def restar(self, producto):
+        id = str(producto.id)
+        if id in self.carrito.keys():
+            self.carrito[id]["cantidad"] -= 1
+            self.carrito[id]["acumulado"] -= producto.precio
+            if self.carrito[id]["cantidad"] <= 0: self.eliminar(producto)
+            self.guardar_carrito()
+    
+    def limpiar(self):
+        self.session["carrito"] = {}
+        self.session.modified = True
--- tienda/models.py
+++ tienda/models.py
@@ -33,3 +33,4 @@ class Producto(models.Model):
 
     def __str__(self):
         return self.nombre
+
