b15fbab66fc79f68297713ffc9c7cdc243472086
OficialNico <112134051+OficialNico@users.noreply.github.com>
2023-06-01 22:54:38 -0400

    Arreglando imagenes

--- core/settings.py
+++ core/settings.py
@@ -134,8 +134,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+IMG_URL = "/tienda/img/"
+IMG_ROOT = os.path.join(BASE_DIR, "tienda/img")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'node_modules/bootstrap/dist'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
