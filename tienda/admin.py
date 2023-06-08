20235e8d0a9aaf2f94482e83a7de53de30fe5922
Pedro Cornejo <pedrocornejo99@gmail.com>
2023-06-07 21:55:06 -0400

    configurado el registro de usuario desde django admin

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,19 +1,33 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm
-from .models import *
+from .models import CustomUser, Marca, Aro, Genero, Producto
 from .forms import CustomUserAdminForm
 
 # Register your models here.
 
-#class CustomUserAdmin(admin.ModelAdmin):
-#    form = CustomUserAdminForm
 
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+#Todos los campos que se pueden manipular en el admin de django
 class CustomUserAdmin(UserAdmin):
-    form = UserChangeForm
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('perfil',)}),
+    form = CustomUserChangeForm
+#    add_fieldsets = (
+#        (None, {
+#            'classes': ('wide',),
+#            'fields': ('username', 'password1', 'password2', 'perfil'),
+#        })),
+    #Campos editables de Usuario
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'perfil')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
+    #Para elegir que campo quiero que se muestre en la tabla de django admin/
+    list_display = ["username", "email", "perfil"]
 
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ["nombre", "precio", "marca"]
@@ -21,12 +35,9 @@ class ProductoAdmin(admin.ModelAdmin):
     list_filter     = ["marca"]
     list_per_page   = 5
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ['username', 'first_name', 'last_name', 'email', 'perfil']
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
-
 admin.site.register(Marca)
 admin.site.register(Aro)
 admin.site.register(Genero)
