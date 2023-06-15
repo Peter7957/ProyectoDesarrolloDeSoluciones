9091854ff172c18ce2e3478631106330ff542438
Pedro Cornejo <pedrocornejo99@gmail.com>
2023-06-15 09:54:37 -0400

    push final

--- core/settings.py
+++ core/settings.py
@@ -92,13 +92,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
     
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+#
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eligetubici',
+        'USER': 'root',
+        'PASSWORD': '',
+
     }
 }
-
 
 
 # Password validation
